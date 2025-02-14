d1c6ac084765a47cce85c6f9361e07e2735487ab
iman <emanmhanafi@gmail.com>
2025-02-14 03:02:55 +0200

    finished authentication with email and password.

---
+++ app/src/main/java/com/example/mealmate/Auth/firebaseAuth/FirebaseAuthHelper.java
@@ -0,0 +1,51 @@
+package com.example.mealmate.Auth.firebaseAuth;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseAuthHelper {
+
+
+    private static FirebaseAuthHelper instance;
+    private FirebaseAuth firebaseAuth;
+
+
+    private FirebaseAuthHelper() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public static synchronized FirebaseAuthHelper getInstance() {
+        if (instance == null) {
+            instance = new FirebaseAuthHelper();
+        }
+        return instance;
+    }
+
+    public void login(String email, String password, FirebaseAuthCallback callback) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess(firebaseAuth.getCurrentUser());
+                    } else {
+                        callback.onFailure(task.getException().getMessage());
+                    }
+                });
+    }
+
+
+    public void signup(String email, String password, FirebaseAuthCallback callback) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess(firebaseAuth.getCurrentUser());
+                    } else {
+                        callback.onFailure(task.getException().getMessage());
+                    }
+                });
+    }
+
+    public void logout() {
+        firebaseAuth.signOut();
+    }
+}
+
