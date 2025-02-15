380d38124c2804a5108478e6ed8bafbf879543a5
iman <emanmhanafi@gmail.com>
2025-02-15 19:55:33 +0200

    finished functionality of daily meal.

--- app/src/main/java/com/example/mealmate/home/view/IHomeView.java
+++ app/src/main/java/com/example/mealmate/home/view/IHomeView.java
@@ -1,4 +1,8 @@
 package com.example.mealmate.home.view;
 
+import com.example.mealmate.model.MealModel;
+
 public interface IHomeView {
+    void showData(MealModel mealModel);
+    void showError(String message);
 }
---
+++ app/src/main/java/com/example/mealmate/network/MealsRemoteDataSourceImpl.java
@@ -0,0 +1,38 @@
+package com.example.mealmate.network;
+
+import com.example.mealmate.model.MealResponse;
+
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import io.reactivex.rxjava3.core.Single;
+
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MealsRemoteDataSourceImpl {
+
+    private static final String BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
+    private static MealsRemoteDataSourceImpl mealsRemoteDataSource = null;
+    ApiService apiService;
+
+    private MealsRemoteDataSourceImpl () {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+        apiService = retrofit.create(ApiService.class);
+    }
+
+
+    public static MealsRemoteDataSourceImpl getInstance() {
+        if (mealsRemoteDataSource == null) {
+            mealsRemoteDataSource = new MealsRemoteDataSourceImpl();
+        }
+        return mealsRemoteDataSource;
+    }
+
+    public Single<MealResponse> getRandomMeal() {
+        return apiService.getRandomMeal();
+    }
+
+
+}
